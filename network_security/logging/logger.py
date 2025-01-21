2a8766b97e3be1edf7e3b5bba426b6de275ac8e9
Ricardo Rojas <ricardorojasm1991@gmail.com>
2025-01-21 17:27:04 -0600

    Exception handling and logging files updated

---
+++ network_security/logging/logger.py
@@ -0,0 +1,19 @@
+import logging
+import os
+from datetime import datetime
+
+# Create file name structure and path
+LOG_FILE = f"{datetime.now().strftime('%d-%m-%Y_%H_%M_%S')}.log"
+LOGS_PATH = os.path.join(os.getcwd(), "logs", LOG_FILE)
+
+# Create a directory to store logs if it does not exist
+os.makedirs(LOGS_PATH, exist_ok=True)
+
+# Create the path for the log files
+LOG_FILE_PATH = os.path.join(LOGS_PATH, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(filename)s - %(message)s",
+)
